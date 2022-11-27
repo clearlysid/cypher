551045bf12a63089de68cfad829f978ea29255c6
Siddharth <siddharth99c@gmail.com>
2022-11-28 01:52:41 +0530

    feat: adds basic structure to about page

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -1,18 +1,49 @@
+import { version, platform } from '@tauri-apps/api/os';
+import { getName, getVersion } from '@tauri-apps/api/app';
 
+
+const appName = await getName();
+const appVersion = await getVersion();
+const osVersion = await version();
+const osPlatform = await platform();
 
 const About = () => {
+
+
+	let osName = ""
+
+	switch (osPlatform) {
+		case 'darwin':
+			osName = "macOS"
+			break;
+		case 'win32':
+			osName = "Windows"
+			break;
+		default:
+			osName = "Linux"
+			break;
+	}
+
+
 	return (
 
 		<div>
-			<h1>About</h1>
+			<div style={{
+				height: 32
+			}}
+				data-tauri-drag-region
+			></div>
+
 			<p>
 				This is a simple app to record your screen.
 			</p>
 			<p>
-				Version: 1.0.0
+				{appName} {appVersion}
+			</p>
+			<p>
+				{osName} {osVersion}
 			</p>
 		</div >
-
 	)
 }
 
