ede39026361b3c6220608383730b279717d5dcc7
Siddharth <siddharth99c@gmail.com>
2022-11-27 18:31:04 +0530

    feat: whiteboard boilerplate

---
+++ src/screens/Whiteboard.tsx
@@ -0,0 +1,15 @@
+
+
+const Whiteboard = () => {
+	return (
+		<div>
+			<h1>Whiteboard</h1>
+			<p>
+				This will likely be a fullscreen adventure
+			</p>
+		</div >
+	)
+}
+
+
+export default Whiteboard
