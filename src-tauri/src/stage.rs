b03c7724e1c4173fb4c3a5beb0137870047109cb
Siddharth <siddharth99c@gmail.com>
2022-11-29 13:20:17 +0530

    feat: adds test recording capabilities

--- src-tauri/src/stage.rs
+++ src-tauri/src/stage.rs
@@ -1,4 +1,6 @@
+// use enigo::Enigo;
 use std::process::Command;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 // ffmpeg -f avfoundation -list_devices true -i ""
 // ffmpeg -f avfoundation -r 30 -s 1920x1080 -i "1:0"  -t 10 ~/test.mp4
@@ -7,14 +9,24 @@ pub fn main() {
     // println!("[dummy] recording started");
     println!("[debug] running ffmpeg");
 
+    let system_time_str = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_millis()
+        .to_string();
+
+    let output_path = format!("/Users/siddharth/desktop/test-{}.mkv", system_time_str);
+
+    let op_str: &str = &&output_path;
+
+    // let cursor_location: (i32, i32) = Enigo::mouse_location();
+
     // let mut cmd = Command::new("ffmpeg")
     //     .args(["-f", "avfoundation", "-list_devices", "true", "-i", ""])
     //     .spawn()
     //     // Blow up if the OS was unable to start the program
     //     .unwrap();
     // let desktop = desktop_dir();
-
-    // println!("{:?}", desktop);
 
     let mut cmd = Command::new("ffmpeg")
         .args([
@@ -28,13 +40,9 @@ pub fn main() {
             "1:0",
             "-t",
             "10",
-            "/Users/siddharth/desktop/test.mp4",
+            op_str,
         ])
         .spawn()
         // Blow up if the OS was unable to start the program
         .unwrap();
-
-    // let mut pwd = Command::new("pwd").spawn().unwrap();
-
-    // println!("{:?}", pwd);
 }
