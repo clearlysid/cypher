0545e23319a61c4630ce8d245c80014334cd1d11
Siddharth <siddharth99c@gmail.com>
2022-12-03 15:16:30 +0700

    feat: adds whiteboard import in app ui

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Stage from "./screens/Stage";
 import About from "./screens/About";
+import Whiteboard from "./screens/Whiteboard";
 import Preferences from "./screens/Preferences";
 import "./style.css";
 
@@ -13,6 +14,8 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
         <Route index element={<Stage />} />
         <Route path="about" element={<About />} />
         <Route path="preferences" element={<Preferences />} />
+        <Route path="stage" element={<Stage />} />
+        <Route path="whiteboard" element={<Whiteboard />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
