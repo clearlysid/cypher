151e89ca26dfa3f4821d7e13bf7055e23a67b17a
Siddharth <siddharth99c@gmail.com>
2022-12-05 14:17:18 +0700

    style: about page restructure

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -4,84 +4,7 @@ import { getName, getVersion } from '@tauri-apps/api/app';
 import { styled } from '@stitches/react';
 
 
-const Container = styled(`div`, {
-	display: 'flex',
-	flexDirection: 'column',
-	alignItems: 'center',
-	height: `calc(100% - 32px)`,
-	backgroundColor: `rgba(25, 25, 25)`
-});
-
-const AppIconPlaceholder = styled('img', {
-	height: 100,
-	width: 100,
-	borderRadius: 16,
-	marginTop: 20,
-	marginBottom: 36,
-	transition: `all 0.2s ease`,
-	border: `2px solid rgba(0, 0, 0, 0.3)`,
-	boxShadow: `0 8px 24px rgba(65, 0, 203, 0.4)`,
-	userSelect: 'none',
-
-	'&:hover': {
-		transform: `scale(1.1) rotate(5deg)`,
-		cursor: `pointer`,
-	}
-});
-
-const AppName = styled('h1', {
-	fontSize: 22,
-	fontWeight: 700,
-	marginBottom: 4,
-	textAlign: 'center',
-	letterSpacing: `-0.04em`
-});
-
-const MetadataSection = styled('div', {
-	marginTop: 24
-});
-
-const MetadataRow = styled('div', {
-	display: 'grid',
-	gridTemplateColumns: '1fr 1fr',
-	columnGap: 12,
-});
-
-const MetadataKey = styled('p', {
-	width: 80,
-	textAlign: 'right',
-	fontSize: 10,
-	lineHeight: 1.4,
-	opacity: 0.4
-});
-
-const MetadataValue = styled('p', {
-	width: 80,
-	fontSize: 10,
-	lineHeight: 1.4,
-	opacity: 1,
-});
-
-const Paragraph = styled('p', {
-	fontSize: 10,
-	lineHeight: 1.4,
-	opacity: 0.4,
-	textAlign: 'center',
-	fontWeight: 400
-});
-
-const Footer = styled('p', {
-	fontSize: 10,
-	lineHeight: 1.4,
-	opacity: 0.4,
-	textAlign: 'center',
-	fontWeight: 400,
-	marginTop: 'auto',
-	marginBottom: 16,
-	width: '85%'
-});
-
-const About = () => {
+export default function About() {
 
 	const [data, setData] = useState({
 		osVersion: '',
@@ -160,5 +83,79 @@ const About = () => {
 	)
 }
 
+const Container = styled(`div`, {
+	display: 'flex',
+	flexDirection: 'column',
+	alignItems: 'center',
+	height: `calc(100% - 32px)`,
+	backgroundColor: `rgba(25, 25, 25)`
+});
 
-export default About
+const AppIconPlaceholder = styled('img', {
+	height: 100,
+	width: 100,
+	borderRadius: 16,
+	marginTop: 20,
+	marginBottom: 36,
+	transition: `all 0.2s ease`,
+	border: `2px solid rgba(0, 0, 0, 0.3)`,
+	boxShadow: `0 8px 24px rgba(65, 0, 203, 0.4)`,
+	userSelect: 'none',
+
+	'&:hover': {
+		transform: `scale(1.1) rotate(5deg)`,
+		cursor: `pointer`,
+	}
+});
+
+const AppName = styled('h1', {
+	fontSize: 22,
+	fontWeight: 700,
+	marginBottom: 4,
+	textAlign: 'center',
+	letterSpacing: `-0.04em`
+});
+
+const MetadataSection = styled('div', {
+	marginTop: 24
+});
+
+const MetadataRow = styled('div', {
+	display: 'grid',
+	gridTemplateColumns: '1fr 1fr',
+	columnGap: 12,
+});
+
+const MetadataKey = styled('p', {
+	width: 80,
+	textAlign: 'right',
+	fontSize: 10,
+	lineHeight: 1.4,
+	opacity: 0.4
+});
+
+const MetadataValue = styled('p', {
+	width: 80,
+	fontSize: 10,
+	lineHeight: 1.4,
+	opacity: 1,
+});
+
+const Paragraph = styled('p', {
+	fontSize: 10,
+	lineHeight: 1.4,
+	opacity: 0.4,
+	textAlign: 'center',
+	fontWeight: 400
+});
+
+const Footer = styled('p', {
+	fontSize: 10,
+	lineHeight: 1.4,
+	opacity: 0.4,
+	textAlign: 'center',
+	fontWeight: 400,
+	marginTop: 'auto',
+	marginBottom: 16,
+	width: '85%'
+});
